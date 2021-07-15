dd4eaee3be94728c12a08c50c024325aa499b183
SammygoodTunes <sygamerz@gmail.com>
2021-07-15 10:46:46 +0200

    Fix weird acceleration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 import pygame
+import random
 
 pygame.init()
 
@@ -22,11 +23,12 @@ class Simulation:
         self.px, self.py = 0, 0
         self.p1_x, self.p1_y = 0, 0
         self.p2_x, self.p2_y = 0, 0
-        self.p1_length, self.p2_length = 100, 100
+        self.p1_length, self.p2_length = 125, 125
         self.p1_mass, self.p2_mass = 10, 10
-        self.p1_angle, self.p2_angle = 0.01, 0
+        self.p1_angle, self.p2_angle = math.pi/random.uniform(1, 8), math.pi/random.uniform(1, 8)
         self.p1_velocity, self.p2_velocity = 0, 0
         self.p1_acceleration, self.p2_acceleration = 0, 0
+        self.prev = [self.px, self.py, self.p1_x, self.p1_y, self.p2_x, self.p2_y]
 
     def set_path_surface(self, window):
         self.path_surface = pygame.Surface(window.get_size())
@@ -55,6 +57,11 @@ def main():
         window.screen.blit(sim.path_surface, (0, 0))
         px, py, p1_x, p1_y = sim.pendulum1(window)
         p2_x, p2_y = sim.pendulum2()
+        if sim.prev[0] != 0:
+            pygame.draw.line(sim.path_surface, (255, 0, 0),
+                             (sim.prev[0] + sim.prev[2] + sim.prev[4], sim.prev[1] + sim.prev[3] + sim.prev[5]),
+                             (px + p1_x + p2_x, py + p1_y + p2_y))
+        sim.prev = [px, py, p1_x, p1_y, p2_x, p2_y]
         pygame.draw.line(window.screen, (255, 255, 255), (px, py), (px + p1_x, py + p1_y))
         pygame.draw.line(window.screen, (255, 255, 255), (px + p1_x, py + p1_y), (px + p1_x + p2_x, py + p1_y + p2_y))
         pygame.draw.ellipse(window.screen, (255, 255, 255),
@@ -63,9 +70,8 @@ def main():
         pygame.draw.ellipse(window.screen, (255, 255, 255),
                             [px + p1_x + p2_x - sim.p2_mass / 2 + 1, py + p1_y + p2_y - sim.p2_mass / 2 + 1,
                              sim.p2_mass, sim.p2_mass])
-        pygame.draw.ellipse(sim.path_surface, (255, 0, 0), [px + p1_x + p2_x, py + p1_y + p2_y, 2, 2])
 
-        g = 0.2
+        g = 0.9806
 
         n1 = -g * (2 * sim.p1_mass + sim.p2_mass) * math.sin(sim.p1_angle)
         n2 = -sim.p2_mass * g * math.sin(sim.p1_angle - 2 * sim.p2_angle)
@@ -81,10 +87,10 @@ def main():
         n5 = sim.p2_length * (2 * sim.p1_mass + sim.p2_mass - sim.p2_mass * math.cos(2 * sim.p1_angle - 2 * sim.p2_angle))
         sim.p2_acceleration = (n1 * (n2 + n3 + n4)) / n5
 
-        sim.p1_angle += sim.p1_velocity
-        sim.p2_angle += sim.p2_velocity
         sim.p1_velocity += sim.p1_acceleration
         sim.p2_velocity += sim.p2_acceleration
+        sim.p1_angle += sim.p1_velocity
+        sim.p2_angle += sim.p2_velocity
 
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
